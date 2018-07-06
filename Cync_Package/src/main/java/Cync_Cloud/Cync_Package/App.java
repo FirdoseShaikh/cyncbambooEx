e9543e8b5ad40f2d68477cd5c887f901b821d4ad
chandramauleshwar <40889677+chandramauleshwar@users.noreply.github.com>
2018-07-06 17:50:14 +0530

    Add files via upload

---
+++ Cync_Package/src/main/java/Cync_Cloud/Cync_Package/App.java
@@ -0,0 +1,13 @@
+package Cync_Cloud.Cync_Package;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
